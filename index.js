c2db650f00aab640854de73152d131103fde8c40
karlteine <karlkivisild@gmail.com>
2024-03-08 11:28:34 +0200

    change tooltip function. added some pictures.

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-function hint() {
-    setTimeout(function() {
-        document.getElementById("hint1").innerHTML = "Hint Hint sssss sssssss Hint Hint Hint";
-    }, 10);
-
-    setTimeout(function() {
-        document.getElementById("hint2").innerHTML = "Hint 2";
-    }, 20);
-
-    setTimeout(function() {
-        document.getElementById("hint3").innerHTML = "Hint 3";
-    }, 30);
-}
-
 function getinPu(formId, ansId , outID){
     var x = document.getElementById(formId);
     var text = "";
