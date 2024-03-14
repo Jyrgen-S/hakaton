efdcdd3306c050580f570babf3bc533344572afa
djcsdj <djcsdj@hotmail.com>
2024-03-14 11:24:35 +0200

    changed index text, changed info text

---
+++ rules.js
@@ -0,0 +1,8 @@
+function displayRules(){
+    var x = document.getElementById("rules");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
